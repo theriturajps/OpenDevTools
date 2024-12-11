3d7ed7d790d36ed474870433b1f6ca928bf60b8e
Ritu Raj <theriturajps@gmail.com>
2024-12-11 23:59:27 +0530

    first commit

---
+++ categories/categories.js
@@ -0,0 +1,67 @@
+const categories = [
+  {
+    name: 'Icons',
+    color: '#2b6dff',
+    nameID: 'icons'
+  },
+  {
+    name: 'Illustrations',
+    color: '#20c928',
+    nameID: 'illustrations'
+  },
+  {
+    name: 'Photos',
+    color: '#d41cf1',
+    nameID: 'photos'
+  },
+  {
+    name: 'Videos',
+    color: '#ef2350',
+    nameID: 'videos'
+  },
+  {
+    name: 'Colors',
+    color: '#ff36a2',
+    nameID: 'colors'
+  },
+  {
+    name: 'Backgrounds',
+    color: '#5345f7',
+    nameID: 'backgrounds'
+  },
+  {
+    name: 'Typography',
+    color: '#ffaa1b',
+    nameID: 'typography'
+  },
+  {
+    name: '3D',
+    color: '#8935ff',
+    nameID: '3d'
+  },
+  {
+    name: 'Libraries',
+    color: '#ff006e',
+    nameID: 'libraries'
+  },
+  {
+    name: 'Blogs',
+    color: '#00a1ff',
+    nameID: 'blogs'
+  },
+  {
+    name: 'Tools',
+    color: '#f97316',
+    nameID: 'tools'
+  },
+  {
+    name: 'Inspirations',
+    color: '#6366f1',
+    nameID: 'inspirations'
+  },
+  {
+    name: 'Components',
+    color: '#21c95f',
+    nameID: 'components'
+  },
+]
